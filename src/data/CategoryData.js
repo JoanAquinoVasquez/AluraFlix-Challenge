68535610aa9593229d4a83ecc08e889dba639db8
joan <joan.aquino.vasquez@gmail.com>
2024-07-16 21:43:58 -0500

    Terminado

---
+++ src/data/CategoryData.js
@@ -0,0 +1,20 @@
+
+const categoryData = [
+  {
+    id: 1,
+    name: "Frontend",
+    primaryColor: "var(--color-frontend)",
+  },
+  {
+    id: 2,
+    name: "Backend",
+    primaryColor: "var(--color-backend)",
+  },
+  {
+    id: 3,
+    name: "Habilidades Blandas",
+    primaryColor: "var(--color-inov-gestao)",
+  }
+];
+
+export default categoryData;
